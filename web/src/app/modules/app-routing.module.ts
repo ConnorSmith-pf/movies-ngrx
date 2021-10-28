6c779363ea52a2debedc6966b39c877ed70dfba8
Connor Smith <connor.github@gmail.com>
2021-10-28 18:38:34 +0100

    Begin movies portion.
    Setup routing.
    Formatting.
    Add basic routes to check working.

--- web/src/app/modules/app-routing.module.ts
+++ web/src/app/modules/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyCounterComponent } from '../components/my-counter/my-counter.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
+import { HomeComponent } from '../components/home/home.component';
 
 const APP_ROUTES: Routes = [
     {
         path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'counter',
         component: MyCounterComponent
     },
     {
--- web/src/app/modules/app.module.ts
+++ web/src/app/modules/app.module.ts
@@ -3,23 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from '../app.component';
 import { StoreModule } from '@ngrx/store';
-import { MyCounterComponent } from '../components/my-counter/my-counter.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../../environments/environment';
-import { counterReducer } from '../state/counter.reducer';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
+import { MenuComponent } from '../components/menu/menu.component';
+import { HomeComponent } from '../components/home/home.component';
+import { MyCounterModule } from '../components/my-counter/my-counter.module';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MyCounterComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        HomeComponent,
+        MenuComponent
     ],
     imports: [
         AppRoutingModule,
         BrowserModule,
-        StoreModule.forRoot({count: counterReducer}),
+        MyCounterModule,
+        StoreModule.forRoot({}),
         StoreDevtoolsModule.instrument({
             maxAge: 25, // Retains last 25 states
             logOnly: environment.production, // Restrict extension to log-only mode
